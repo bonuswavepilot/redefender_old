2487f069878e5a590ee0cece0822bc5e96929d7c
bonuswavepilot <bwp@bafflegab.org>
2015-10-14 21:07:11 +1100

    Initial commit.  Standard libGDX project as generated by the setup tool with the following options enabled: PLATFORMS: Desktop, Android, Html
    EXTENSIONS:
    Tools, Controllers, Box2d, Box2dlights
    
    3RD PARTY EXTENSIONS:
    libgdx-utils, libgdx-utils-box2d
    
    With generation of project files enabled for Eclipse and Intellij IDEA.

---
+++ desktop/src/digital/bonuswave/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package digital.bonuswave.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import digital.bonuswave.ReDefender;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ReDefender(), config);
+	}
+}
